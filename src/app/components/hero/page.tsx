4abffa66c7a9ac3cd40b97784f6bad88242840b8
DouglasLindahl <douglas.lindahl@gmail.com>
2026-04-03 16:09:31 -0700

    a

--- src/app/components/hero/page.tsx
+++ src/app/components/hero/page.tsx
@@ -1,13 +1,8 @@
-import ColumnaBackground from "../columnaBackground/page";
 import ColumnIcon from "../columnIcon/page";
 
 export default function Hero() {
   return (
     <section className="bg-bg-primary h-screen flex flex-col justify-center items-center gap-[140px] ">
-      <ColumnaBackground></ColumnaBackground>
-      <div className="absolute inset-0 flex items-center justify-center">
-        <ColumnIcon className="w-full h-full" opacity={0.05} />
-      </div>
       <section className="text-center flex flex-col justify-center items-center gap-[24px] text-text-primary w-[60%]">
         <h1>Columna Group</h1>
         <h2>Vi bygger strukturen bakom hållbara företag</h2>
